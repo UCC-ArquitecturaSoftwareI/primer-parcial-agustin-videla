54d14f645fe70c9a8558ef6f73f533ea2a600b56
facundo <ribettif@gmail.com>
2020-04-22 01:27:07 -0300

    Elimine la carpeta que use para la prueba del mapa

--- clases/Nueva carpeta/player.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="player" tilewidth="100" tileheight="128" tilecount="40" columns="8">
- <image source="personaje/Zombie/Tilesheet/character_zombie_sheet.png" width="864" height="640"/>
-</tileset>
