cb2bdf6a30925794da3fd38002c4013b465c84f4
agus98 <agustinvidela0198@gmail.com>
2020-05-17 13:43:19 -0300

    Corregido bug que hacia que la funcion pusiera los bloques mal para coordenadas negativas

--- clases/Vector2Functions/VectorTransform.cpp
+++ clases/Vector2Functions/VectorTransform.cpp
@@ -8,9 +8,13 @@ Vector2 vectorTransform(Vector2 v) {
     Player &player = Player::getInstance();
     v = {player.cage.x + v.x - GetScreenWidth()/2 + player.cage.width/2,
          player.cage.y + v.y - GetScreenHeight()/2 + player.cage.height/2};
-    if((int)v.x%(int)Block::getSize().x != 0)
+    if(v.x >= 0)
         v.x -= (int)v.x%(int)Block::getSize().x;
-    if((int)v.y%(int)Block::getSize().y != 0)
+    else
+        v.x -= Block::getSize().x + (int)v.x%(int)Block::getSize().x;
+    if(v.y >= 0)
         v.y -= (int)v.y%(int)Block::getSize().y;
+    else
+        v.y -= Block::getSize().y + (int)v.y%(int)Block::getSize().y;
     return v;
 }
