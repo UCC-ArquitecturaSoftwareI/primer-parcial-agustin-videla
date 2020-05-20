1ffc99028e8e5db319aaf46546b4c389eaaba3f3
facuribetti <37404884+facuribetti@users.noreply.github.com>
2020-05-20 22:58:44 +0000

    Deploying to gh-pages from  - b9b5fa16ffe05fe1776e5c81f0c7e7d4947fb381 🚀

---
+++ resources/Mapa/SingletonMapa.h
@@ -0,0 +1,85 @@
+//
+// Created by ribet on 18/5/2020.
+//
+
+#ifndef SQUARECRAFT_SINGLETONMAPA_H
+#define SQUARECRAFT_SINGLETONMAPA_H
+
+#include <string>
+#include <raylib.h>
+#include "../../Libraries/tileson.hpp"
+
+//SingletonMapa &mapa = SingletonMapa::getInstance("../resources/Mapa/EntitledMap1.json");
+
+class SingletonMapa {
+private:
+    tson::Map map;
+    Texture2D map_tex;
+    tson::Tileset *map_tileset;
+    SingletonMapa(const std::string file)
+    {
+        std::cout<<"Fuck \n";
+        tson::Tileson parser;
+        //tson::Map map = parser.parse(fs::path("../resources/Mapa/EntitledMap1.json"));
+        map = parser.parse("../resources/Mapa/EntitledMap1.json");
+        if(map.getStatus() == tson::ParseStatus::OK) {
+            for (auto &tileset : map.getTilesets()){
+                std::cout<<"Fuck \n";
+                map_tex = LoadTexture("../resources/Mapa/totatilly-not-minecraft-textures.png");
+                //map_tex = LoadTexture(fs::path("resources/Mapa/" + tileset.getImage().string()).c_str());
+                map_tileset = &tileset;
+            }
+
+            auto objs = map.getLayer("Objetos"); //obtengo la capa objetos
+            //tson::Object *player = objs->firstObj("Player"); //obtengo los datos del jugador
+            //player_init_pos.x = player->getPosition().x;
+            //player_init_pos.y = player->getPosition().y;
+
+            //std::cout<<"posicion x: " << player->getPosition().x;
+            //std::cout<<"posicion y: " << player->getPosition().y;
+
+            for(auto &obj : objs->getObjects()){
+                //revisa todos los objetos
+            }
+        }
+
+    }
+    int getPos (int a, int b, char c){
+        if(a < 17)
+            if(c == 'x')
+                return a;
+            else
+                return b;
+        else
+            getPos (a - 16, b + 1, c);
+    }
+
+public:
+    static SingletonMapa &getInstance(const std::string file){
+        static SingletonMapa mapa(file);
+        return mapa;
+    }
+    Texture2D getTexture(){
+        return map_tex;
+    }
+    Rectangle getRec(int Gid){ //esta es la posicion y tamaño de la textura individual a dibujar
+        Rectangle tile_rec;
+
+        if(Gid < 16)
+            tile_rec.x = (16.0f) * Gid;
+        else
+            tile_rec.x = (16.0f) * getPos(Gid, 0, 'x');
+
+        if(Gid < 16)
+            tile_rec.y = 0.0f;
+        else
+            tile_rec.y = (16.0f) * getPos(Gid, 0, 'y');
+
+        tile_rec.width = map.getTileSize().x;
+        tile_rec.height = map.getTileSize().y;
+        return tile_rec;
+    }
+};
+
+
+#endif //SQUARECRAFT_SINGLETONMAPA_H
