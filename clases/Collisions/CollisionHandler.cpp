e1a86e9f0767f815d44ea610ebf5b910395a3950
facundo <ribettif@gmail.com>
2020-06-02 15:18:11 -0300

    no se cargo todo

--- clases/Collisions/CollisionHandler.cpp
+++ clases/Collisions/CollisionHandler.cpp
@@ -12,14 +12,16 @@ void CollisionHandler::handle(std::vector<int> c) {
     for (auto i : c) {
         switch (i) {
             //arriba-izquierda
-            //case 0:
-                //player.cage.x += 1*player.getSpeed().x;
-                //break;
+            case 0:
+                player.cage.x += 1*player.getSpeed().x;
+                player.cage.y += 1*player.getSpeed().y;
+                break;
             case 1:
                 player.cage.y += 1*player.getSpeed().y;
                 break;
-            //case 2:
+            case 2:
                 player.cage.x += 1*player.getSpeed().x;
+                player.cage.y += 1*player.getSpeed().y;
                 break;
             case 3:
                 player.cage.x -= 1*player.getSpeed().x;
@@ -27,14 +29,16 @@ void CollisionHandler::handle(std::vector<int> c) {
             case 4:
                 player.cage.x -= 1*player.getSpeed().x;
                 break;
-            //case 5:
-                player.setSpeed({0,0});
+            case 5:
+                player.cage.x -= 1*player.getSpeed().x;
+                player.cage.y -= 1*player.getSpeed().y;
                 break;
             case 6:
                 player.cage.y -= 1*player.getSpeed().y;
                 break;
-            //case 7:
-                player.setSpeed({0,0});
+            case 7:
+                player.cage.x -= 1*player.getSpeed().x;
+                player.cage.y -= 1*player.getSpeed().y;
                 break;
             case 8:
                 player.cage.x += 1*player.getSpeed().x;
